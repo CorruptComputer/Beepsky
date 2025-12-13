5529681e1debebd629ed1b1c1a54f27795822ca7
Nickolas Gupton <nickolas@gupton.xyz>
2025-12-12 18:29:49 -0600

    Fix install issue with config

--- update-dotnet-tools.sh
+++ update-dotnet-tools.sh
@@ -3,11 +3,8 @@
 dotnet tool update --global --all
 dotnet workload update
 
-cd Beepsky.Core
+cd Beepsky/
 dotnet tool update dotnet-ef
-dotnet tool restore
-
-cd ../Beepsky/
 dotnet tool update dotnet-rpm
 dotnet tool update dotnet-deb
 dotnet tool restore
